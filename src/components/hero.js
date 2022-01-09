f1bb650e5ebf19da7078012876b1d550a7a20a51
Prohashim <prohashim4e@gmail.com>
2022-01-10 00:08:38 +0500

    3rd day

---
+++ src/components/hero.js
@@ -0,0 +1,21 @@
+// function header component
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-container">
+        <section className="hero-title">
+          <h1 className="hero-heading">
+            <span className="hero-heading-span">Let's Find</span>
+            <span className="hero-heading-span-1">yours Dream House</span>
+          </h1>
+          <p className="hero-sub-heading">
+            Your own home is just a click away in Dubai 💙
+          </p>
+        </section>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import Navbar from './components/header';
+import Hero from './components/hero';
+
+
+function Home(){
+  return(
+    <>
+    <Navbar />
+    <Hero />
+    </>
+  )
+}
 
 
 ReactDOM.render(
-  <Navbar />,
+  <Home />,
+
   document.getElementById('root')
 );
 
